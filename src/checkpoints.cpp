bf4f66a059b24bc94316804f03f3a9f8042e8ec5
happy <bumbacoin@gmail.com>
2017-09-15 21:59:01 +0800

    checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -28,7 +28,10 @@ namespace Checkpoints
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
         ( 0,      hashGenesisBlock )
+        ( 1,      0x00000147a6ca0811a644cbf7c70843ea9954a8fd676bac988d8c0a1eed73e631 )
+        ( 7,      0x0000021b0aae1934a9cd257a6d9439dfb29ceeec2948a6fdfd7e224521409f9b )
     ;
+
 
     // TestNet has no checkpoints
     static MapCheckpoints mapCheckpointsTestnet =
