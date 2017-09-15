127d5579de74add8ebbd3e6c2c369bf746d9b329
happy <bumbacoin@gmail.com>
2017-09-15 23:26:28 +0800

    nodes

--- src/net.cpp
+++ src/net.cpp
@@ -1229,7 +1229,8 @@ void MapPort()
 // The first name is used as information source for addrman.
 // The second name should resolve to a list of seed addresses.
 static const char *strDNSSeed[][2] = {
-    {"goku1:dynu.net", "goku1:dynu.net"},
+    {"goku1.dynu.net", "goku1.dynu.net"},
+    {"23.152.0.160", "23.152.0.160"},
 };
 
 void ThreadDNSAddressSeed(void* parg)
