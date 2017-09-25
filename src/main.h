ab925358bffae9abd9c529cc5c51eb07059c6c85
happy <bumbacoin@gmail.com>
2017-09-25 11:21:39 +0800

    reinstate pow

--- src/main.h
+++ src/main.h
@@ -28,7 +28,9 @@ class CNode;
 
 class CTxMemPool;
 
-static const int LAST_POW_BLOCK = 10000;
+static const int LAST_POW_BLOCK_V1 = 10000;
+static const int POW_RE_ENABLE = 14000;  
+static const int LAST_POW_BLOCK = 9999999999;
 
 /** The maximum allowed size for a serialized block, in bytes (network rule) */
 static const unsigned int MAX_BLOCK_SIZE = 1000000;
