da99abebeefd14e47e07ed91b797f0f727018406
bumbacoin <bumbacoin@gmail.com>
2017-11-21 19:53:41 +0800

    prepare for v22 fork

--- src/version.h
+++ src/version.h
@@ -30,14 +30,14 @@ static const int DATABASE_VERSION = 70509;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 60018;
+static const int PROTOCOL_VERSION = 60019;
 
 // intial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 // disconnect from peers older than this proto version
 static const int MIN_PROTO_VERSION = 60017;  // v2
-static const int MIN_PROTO_VERSION_FORK = 60018; // v2.1 up
+static const int MIN_PROTO_VERSION_FORK = 60019; // v2.2 up
 
 
 // nTime field added to CAddress, starting with this version;
@@ -47,7 +47,7 @@ static const int CADDR_TIME_VERSION = 31402;
 // only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 60002;
 static const int NOBLKS_VERSION_END = 60016;
-static const int NOBLKS_VERSION_END_FORK = 60017; // share nodes but no blocks
+static const int NOBLKS_VERSION_END_FORK = 60018; // share nodes but no blocks
 
 // BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
