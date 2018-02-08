1f093cd4f0fbdbd687abce6fa408ffbe5706fe67
bumbacoin <bumbacoin@gmail.com>
2018-02-08 15:25:23 +0800

    fix the fix

--- src/main.cpp
+++ src/main.cpp
@@ -1064,7 +1064,7 @@ int64_t GetProofOfStakeReward(int nHeight, int64_t nCoinAge, int64_t nFees)
 	
             else if (nBestHeight <= 10000)
             {
-            nSubsidy = nCoinAge * COIN_YEAR_REWARD * 33 / (365 * 33 + 8) * 5 ;  //5000% until block 10k
+            nSubsidy = nCoinAge * COIN_YEAR_REWARD * CENT * 33 / (365 * 33 + 8) * 5 ;  //5000% until block 10k
             }
 	
 	    else if (nBestHeight <= 14000)
